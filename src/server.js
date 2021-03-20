ec8d2c7e8e923f05ccc9b3c959c60f61787e17c3
SamuelGuimaraesCosta <75049214+SamuelGuimaraesCosta@users.noreply.github.com>
2021-03-20 17:31:16 -0300

    router

--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ var http = require('http');
 var port = process.env.PORT || 3333;
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors");
 const sqlite3 = require("sqlite3").verbose();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const mysql = require('mysql');
 const fs = require("fs");
+
+const app = express();
+const router = express.Router();
 
 var conn = mysql.createConnection({
     host: "pesquisamysql.mysql.database.azure.com",
