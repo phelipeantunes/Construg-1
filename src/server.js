d3355c8904c15b3f46c5f5a02874d2b274aab716
git config --global user.name Your <phelipe.antunes@outlook.com>
2021-03-22 19:14:48 -0300

    teste

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ var conn = mysql.createConnection({
 });
 
 router.get('/teste', (req, res) => {
-    res.status(200).send("AAAAAAAAAAA! OUTRO COMMIT TESTE");
+    res.status(200).send("xsbjbxsjk! OUTRO COMMIT TESTE");
 });
 
 router.get('/', (req, res) => {
